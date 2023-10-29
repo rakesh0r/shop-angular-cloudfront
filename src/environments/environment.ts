4cf0f62f4d84b411283a378b150c8ec53327bc64
Rakesh Rekala <rakesh_rekala@epam.com>
2023-10-29 13:45:59 +0530

    Task 5 | Integration with S3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,14 +9,14 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://xzwc6a7y7g.execute-api.us-east-1.amazonaws.com/dev',
     bff: 'https://aeeugzrdll.execute-api.us-east-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
     product: false,
     order: false,
-    import: false,
+    import: true,
     bff: true,
     cart: false,
   },
